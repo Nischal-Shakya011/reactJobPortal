d44f7f3a51a77c94fe6d47be18f62ac6f6856309
nischal <shakyanischal1111@gmail.com>
2024-03-14 21:59:49 +0545

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer(){
 
     return(
         <div className='bg-primary p-3 text-center mt-5 '>
-<p className=' text-white'> <AiFillCopyrightCircle className='inline'/> 2024 JobHunt. Designed By Nischal Shakya, Roshan Shrestha and Dharmapal Karki </p>
+<p className=' text-white'> <AiFillCopyrightCircle className='inline'/> 2024 JobHunt Project. Designed By Nischal Shakya, Roshan Shrestha and Dharmapal Karki </p>
         </div>
     )
 }
